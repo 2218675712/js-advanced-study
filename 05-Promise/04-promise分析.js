3b8839f5fe1b5854dd73df99baf96d863697c2cb
2218675712 <2218675712@qq.com>
2020-06-02 23:28:14 +0800

    2020年06月02日23:28:06 手写promise 异步

--- 05-Promise/04-promise分析.js
+++ 05-Promise/04-promise分析.js
@@ -17,8 +17,8 @@ console.log(3);
 */
 
 /*
-
-let Promise = require('./promise');
+产生错误,查看异常是否处理
+let Promise = require('./05-手写promise');
 let p1 = new Promise((resolve,reject)=>{
     //resolve(123);
     //reject(11);
@@ -30,10 +30,11 @@ p1.then((result)=>{
 },(reason)=>{
     console.log(11111);
     console.log(reason);
-});
+});*/
 //console.log(p1.status);
-*/
-let promise = require('./05-手写promise');
+/*
+同步代码
+let Promise = require('./05-手写promise');
 let p1 = new Promise((resolve, reject) => {
     // 只能执行一个
     reject(11);
@@ -46,4 +47,26 @@ p1.then(data => {
 })
 
 console.log(333);
+*/
 
+
+let Promise = require('./05-手写promise');
+let p1 = new Promise((resolve, reject) => {
+/*        setTimeout(() => {
+            resolve("ok");
+        }, 2000);*/
+        setTimeout(() => {
+            reject("error");
+        }, 2000);
+    }
+)
+p1.then(data => {
+    console.log(data+"11");
+}, error => {
+    console.log(error);
+})
+p1.then(data => {
+    console.log(data+"22");
+}, error => {
+    console.log(error);
+})
--- 05-Promise/05-手写promise.js
+++ 05-Promise/05-手写promise.js
@@ -5,9 +5,16 @@ class Promise {
         // 因为箭头函数没有this(所有this是它上级的所处环境),所有临时保存下来
         let _this = this;
         // 定义当前等待状态
-        this.status = 'pending';
-        _this.value = ''; // 记录成功原因
-        _this.reason = undefined; // 记录失败的原因
+        _this.status = 'pending';
+        // 记录成功原因
+        _this.value = '';
+        // 记录失败的原因
+        _this.reason = undefined;
+        // 存放成功回调
+        _this.onResolvedCallbacks = [];
+        // 存放失败回调
+        _this.onRejectCallbacks = [];
+
         // 定义函数(成功失败)
         function resolve(value) {
             // 状态只能从等待向成功或者失败转
@@ -16,6 +23,8 @@ class Promise {
                 _this.status = 'resolved';
                 // 保存成功的数据
                 _this.value = value;
+                // 依次执行回调函数
+                _this.onResolvedCallbacks.forEach(item => item(_this.value));
             }
         }
 
@@ -27,6 +36,8 @@ class Promise {
                 _this.status = 'reasoned';
                 // 保存失败的原因
                 _this.reason = reason;
+                // 依次执行回调函数
+                _this.onRejectCallbacks.forEach(item => item(_this.reason));
             }
         }
 
@@ -42,6 +53,14 @@ class Promise {
 
     // then传递成功或失败回调函数
     then(onFulfilled, onRejected) {
+
+        // 如果状态为等待
+        if (this.status === 'pending') {
+            // 将成功或者失败函数保存到数组中
+            this.onResolvedCallbacks.push(onFulfilled);
+            this.onRejectCallbacks.push(onRejected);
+        }
+
         // 如果状态为成功
         if (this.status === 'resolved') {
             // 执行成功函数，把数据传过去
