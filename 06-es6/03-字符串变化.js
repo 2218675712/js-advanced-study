660a1093efa93bbb7e458e64a4ca90654254d75a
2218675712 <2218675712@qq.com>
2020-06-08 10:03:00 +0800

    2020年06月08日10:02:54 字符串api

--- 06-es6/03-字符串变化.js
+++ 06-es6/03-字符串变化.js
@@ -29,9 +29,26 @@ str = str.replace(/\$\{([^{]+)\}/g, function () {
 console.log(str);
 */
 // 案例:生成列表
+/*
+
 let arr = ["苹果", "香蕉", "梨子"];
 // map遍历每个元素
 let lis = arr.map(item => `<li>${item}</li>`);
 // join放入一个字符串
 let ul = `<ul>${lis.join('')}</ul>`;
-console.log(ul)
+console.log(ul);
+*/
+
+// 新增常用api
+// 判断开头
+console.log("http://www.baidu.com/".startsWith("http"));
+// 判断结尾
+console.log("1.png".endsWith("jpeg"));
+// 判断是否含有
+console.log('welcome to beijing'.includes('to'));
+// 重复n次
+console.log('hi'.repeat(10));
+// 开头补数字
+console.log('7时'.padStart(3, 0));
+// 末尾补数字
+console.log('0'.padEnd(8, 5));
